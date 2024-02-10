e0ad6df077b507f1f1aabb396c880f021aa2dfe6
petouha <rachidgames2.rg@gmail.com>
2024-02-10 21:04:48 +0100

    search function return changed

--- functions.h
+++ functions.h
@@ -17,7 +17,7 @@ typedef struct sym
 
 sym_tab *nouvelle_cellule();
 void ajouter(int val, char* nom, sym_tab** head);
-int recherche(char* nom, sym_tab* head);
+sym_tab *recherche(char* nom, sym_tab* head);
 void print_sym_tab(sym_tab *head);
 
 void addition();
