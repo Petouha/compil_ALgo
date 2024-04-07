295aca779e3dc04ae3c07971c6b0e9421a8811da
Petouha <rachidgames2.rg@gmail.com>
2024-04-07 17:27:43 +0200

    added full implementation

--- intermediare.c
+++ intermediare.c
@@ -1,8 +1,9 @@
 #include "intermediare.h"
+#include "functions.h"
 
 //corps des fonctions
 
-void add_intermediare(intermediare **head, operation op, arg_type type, char *arg){
+void add_intermediare(intermediare **head, operation op, arg_type type, char *arg, func_tab* func){
     intermediare *new = (intermediare *)malloc(sizeof(intermediare));
     fprintf(stderr,"je vais ajouter %d\n",op);
     if(new == NULL){
@@ -17,6 +18,7 @@ void add_intermediare(intermediare **head, operation op, arg_type type, char *ar
     }
     
     new->op = op;
+    new->function = func;
     new->next = NULL;
     if(*head == NULL){
         *head = new;
@@ -28,6 +30,28 @@ void add_intermediare(intermediare **head, operation op, arg_type type, char *ar
         current->next = new;
     }
 }
+
+
+void generate_intermediare(intermediare *head){
+    start_asm();
+    while(head != NULL){
+        choose_op(head);
+        head = head->next;
+    }
+}
+
+void free_intermediare(intermediare *head){
+    intermediare *current = head;
+    while(current != NULL){
+        intermediare *tmp = current;
+        current = current->next;
+        if(tmp->type == ARG){
+            free(tmp->arg);
+        }
+        free(tmp);
+    }
+}
+
 
 void print_intermediare(intermediare *head){
     fprintf(stderr,"------------\n");
@@ -41,4 +65,73 @@ void print_intermediare(intermediare *head){
         head=head->next;
     }
     fprintf(stderr,"------------\n");
+}
+
+
+void choose_op(intermediare *node){
+    switch(node->op){
+        case ADD_OP:
+            addition();
+            break;
+        case SUB_OP:
+            soustraction();
+            break;
+        case MUL_OP:
+            multiplication();
+            break;
+        case DIV_OP:
+            division();
+            break;
+        case AND_OP:
+            multiplication();
+            break;
+        case OR_OP:
+            fprintf(stderr,"OR_OP\n");
+            break;
+        case NOT_OP:
+            fprintf(stderr,"NOT_OP\n");
+            break;
+        case SET_OP:
+            affect_from_top_stack(node->arg,node->function);
+            break;
+        case INCR_OP:
+            increment(node->arg,node->function);
+            break;
+        case DECR_OP:
+            decrement(node->arg,node->function);
+            break;
+        case IDF_OP:
+            get_param_from_stack(node->arg,node->function);
+            break;
+        case NUM_OP:
+            num(atoi(node->arg));
+            break;
+        case NUL_OP:
+            break;
+        case FUNC_OP:
+            printf(":%s\n",node->function->nom_func);
+            prepare_stack_func(node->function);
+            break;
+        case CALL_OP:
+            main_asm();
+            break;
+        case CALLEND_OP:
+            printf(";callend\n");
+            prepare_stack_params(node->function);
+            prepare_stack_locals(node->function);
+            printf("\tconst ax,%s\n",node->function->nom_func);
+            printf("\tcall ax\n");
+            printf(";end callend\n");
+            printf(";Dépiler le nombre de variables locales + paramètres\n");
+            for (int i = 0; i < node->function->nbr_locals + node->function->nbr_params; i++){
+            printf("\tpop ax\n");
+        }
+            end_asm();
+            break;
+        case RET_OP:
+            return_from_func(node->function,node->arg);
+            break;
+        default:
+            break;
+    }
 }
--- intermediare.h
+++ intermediare.h
@@ -1,28 +1,33 @@
 // un fichier .h qui contient la structure de la liste chainée et les prototypes 
 // des fonctions qui permettent de manipuler cette liste du code intermédiaire
 // l'insertion se fait en fin de liste (FIFO)
-
+#ifndef INTERMEDIAIRE_H
+#define INTERMEDIAIRE_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "functions.h"
 // enumération des types des opérations
 
 typedef enum operation{
-    ADD_OP,
-    SUB_OP,
-    MUL_OP,
-    DIV_OP,
-    AND_OP,
-    OR_OP,
-    NOT_OP,
-    SET_OP,
-    INCR_OP,
-    DECR_OP,
-    IDF_OP,
-    NUM_OP,
-    NUL_OP,
-    FUNC_OP,
+    ADD_OP, //0
+    SUB_OP, //1
+    MUL_OP, //2
+    DIV_OP, //3
+    AND_OP, //4
+    OR_OP, //5
+    NOT_OP, //6
+    SET_OP, //7
+    INCR_OP, //8
+    DECR_OP, //9
+    IDF_OP, //10
+    NUM_OP, //11
+    NUL_OP, //12
+    FUNC_OP, //13
+    CALL_OP, //14
+    CALLEND_OP, //15
+    RET_OP, //16
+    TEST_OP, //17
 } operation;
 
 // enum pour différencier entre les deux types d'arguments
@@ -39,6 +44,7 @@ typedef struct intermediare{
     operation op;
     arg_type type;
     char *arg;
+    func_tab *function;
     struct intermediare *next;
 } intermediare;
 
@@ -51,10 +57,11 @@ argumetns:
     - arg1: l'argument de l'opération
 
 */
-void add_intermediare(intermediare **head, operation op,arg_type type, char *arg);
+void generate_intermediare(intermediare* head);
+void add_intermediare(intermediare **head, operation op,arg_type type, char *arg,func_tab* func);
 void print_intermediare(intermediare *head);
+void choose_op(intermediare *node);
+void free_intermediare(intermediare *head);
 
 
-
-
-
+#endif
